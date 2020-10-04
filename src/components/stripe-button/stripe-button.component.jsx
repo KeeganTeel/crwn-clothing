3e22941cc7589112d4af56151f39f13ac801fe9f
Keegan Teel <keeganfteel@yahoo.com>
2020-10-04 14:30:14 -0400

    uploading app

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HXyEwBxpEKJ4XRRkqOWArw2tF1lEbdxkI6iKNAB1E3Cq6HIGlKnFZZYtZtQNlNxZ3mR1aWsib41Hgx2jAmvRbAs00kKCelnvk';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
